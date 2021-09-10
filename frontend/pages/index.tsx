d1d48aaecc61f01dd75089edc2c1f59dfa9acfd2
nullworkbench <nullworkbench@gmail.com>
2021-09-11 01:42:11 +0900

    ログインボタンのパスを設定

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
             Welcome to <span className="text-white">OcOsu</span>
           </h1>
 
-          <Link href="/">
+          <Link href="/auth">
             <a className="block w-max bg-white rounded-md px-6 py-2">Login</a>
           </Link>
         </div>
