b3525f37322e3e0365a04ce084d017637f91703b
nullworkbench <nullworkbench@gmail.com>
2021-09-10 16:23:04 +0900

    uninstall chakraUI

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,8 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
+
+// import Chakra UI
+
+// global style
 import "../styles/globals.scss";
 
 const App = ({ Component, pageProps }: AppProps) => {
