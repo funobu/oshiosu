b034b93acbfb56c37c3592e7e7a0094f8debf79c
nullworkbench <nullworkbench@gmail.com>
2021-09-15 22:07:45 +0900

    ユーザーページに推しを表示するように

--- frontend/pages/users/[uid].tsx
+++ frontend/pages/users/[uid].tsx
@@ -32,32 +32,49 @@ const UserPage: NextPage<{ user: User }> = ({ user }) => {
     <div className="container mx-auto relative h-screen">
       <main className="h-full">
         <h2 className="text-3xl text-center pt-48">ユーザー情報</h2>
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-          <div
-            className="w-4/5 max-w-4xl p-8 border-8 border-white rounded-3xl bg-yellow-300 shadow-xl"
-            style={{
-              backgroundImage: `url(
+
+        {/* OcOsu Passport */}
+        <div
+          className="w-4/5 max-w-4xl mx-auto mt-16 p-8 border-8 border-white rounded-3xl bg-yellow-300 shadow-xl"
+          style={{
+            backgroundImage: `url(
                     "data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23f7f7f7' fill-opacity='0.8' fill-rule='evenodd'%3E%3Cpath d='M0 40L40 0H20L0 20M40 40V20L20 40'/%3E%3C/g%3E%3C/svg%3E"
                   )`,
-            }}
-          >
-            <div className="p-10 rounded-3xl border-4 border-white bg-yellow-300 bg-opacity-90">
-              <h3 className="text-5xl font-bold text-white">OcOsu Passport</h3>
-              <div className="flex items-center w-full mt-8 mb-2">
-                <div className="flex-none w-1/3">
-                  <img src={user.icon} alt="" />
-                </div>
-                <div className="flex-grow ml-12">
-                  <h3 className="text-4xl font-semibold mb-2">
-                    {user.username}
-                  </h3>
-                  <p className="text-xl">{user.email}</p>
-                </div>
+          }}
+        >
+          <div className="p-10 rounded-3xl border-4 border-white bg-yellow-300 bg-opacity-90">
+            <h3 className="text-5xl font-bold text-white">OcOsu Passport</h3>
+            <div className="flex items-center w-full mt-8 mb-2">
+              <div className="flex-none w-1/3">
+                <img src={user.icon} alt="" />
+              </div>
+              <div className="flex-grow ml-12">
+                <h3 className="text-4xl font-semibold mb-2">{user.username}</h3>
+                <p className="text-xl">{user.email}</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="absolute bottom-24 w-full">
+
+        {/* Your Oc */}
+        <div className="mt-24 text-center">
+          <h3 className="text-3xl font-semibold mb-8">Your Oc</h3>
+          <div className="bg-white px-8 py-8 rounded-2xl shadow-xl mb-10">
+            <img
+              src={user.icon}
+              className="mx-auto mb-8"
+              alt=""
+              style={{ maxHeight: "500px" }}
+            />
+            <span className="block text-2xl font-semibold text-yellow-600 mb-4">
+              Title
+            </span>
+            <p>Explanation</p>
+          </div>
+        </div>
+
+        {/* Home Btn */}
+        <div className="mt-32 pb-48">
           <Link href="/" passHref>
             <a className="block w-max mx-auto px-12 py-3 rounded-xl border-4 border-yellow-300 text-xl font-semibold hover:bg-yellow-300 hover:border-white hover:text-white transition">
               Home
