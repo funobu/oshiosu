f3f54a4d6c99e8792a2526505f7fca95b3c55012
nullworkbench <nullworkbench@gmail.com>
2021-09-17 13:24:50 +0900

    login.tsx スタイリング

--- frontend/pages/auth/login.tsx
+++ frontend/pages/auth/login.tsx
@@ -55,14 +55,36 @@ const Login: NextPage = () => {
     );
   };
 
+  const FormInput = ({
+    type,
+    placeholder,
+  }: {
+    type: keyof Form;
+    placeholder: string;
+  }) => {
+    return (
+      <input
+        type={type}
+        {...register(type)}
+        placeholder={placeholder}
+        className="block mx-auto w-full max-w-lg bg-white rounded-2xl shadow-lg outline-none px-8 py-4 mb-8"
+      />
+    );
+  };
+
   return (
-    <div className="container mx-auto relative h-screen">
-      <main className="h-full">
-        <h2 className="text-3xl text-center pt-48">Login</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("email")} />
-          <input {...register("password")} />
-          <input type="submit" />
+    <div className="container mx-auto min-h-screen">
+      <main>
+        <h2 className="text-3xl text-center pt-48">Login to OcOsu</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-14">
+          <FormInput type="email" placeholder="メールアドレスを入力" />
+          <FormInput type="password" placeholder="パスワードを入力" />
+          <button
+            type="submit"
+            className="block mx-auto mt-14 font-semibold bg-yellow-400 text-white px-16 py-3 rounded-xl shadow-lg"
+          >
+            ログイン
+          </button>
         </form>
         <button onClick={getUser}>取得</button>
       </main>
